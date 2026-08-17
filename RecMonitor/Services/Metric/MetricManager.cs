ba54f1b4f9b804b944407b250f06b5b98ef3d2e4
whgds1360 <maxprizrac@gmail.com>
2026-08-17 12:10:26 +0300

    Добавлены методы по сбору информации с датчиком видеокарты и процессора

---
+++ RecMonitor/Services/Metric/MetricManager.cs
@@ -0,0 +1,46 @@
+﻿using LibreHardwareMonitor.Hardware;
+using Services.Metric.UpdateVisitor;
+
+namespace RecMonitor.Services.Metric
+{
+    internal class MetricManager
+    {
+        private Computer? computer; 
+
+        public MetricManager()
+        {
+            try
+            {
+                this.computer = new Computer
+                {
+                    IsCpuEnabled = true,
+                    IsGpuEnabled = true,
+                    IsMemoryEnabled = true,
+                };
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show($"Ошибка инициализации сборщика метриков: {error.Message}");
+            }
+        }
+
+        public Dictionary<string, string>? getMetricInfo()
+        {
+            try
+            {   
+                if (this.computer is null)
+                {
+                    return null;
+                    throw new ArgumentNullException("Ошибка инициализации сборщика метриков");
+                }
+                this.computer.Open();
+                this.computer.Accept(new UpdateVisitor());
+            }
+
+            catch (Exception error)
+            {
+                MessageBox.Show($"Ошибка: {error.Message}");
+            }
+        }
+    }
+}
