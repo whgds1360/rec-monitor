84681ce51fd882e7b770d797e2031885c0cd1d04
whgds1360 <maxprizrac@gmail.com>
2026-09-01 12:08:37 +0300

    Закончил с реализацией логики но пока отображении актуальных данных не работает

--- SystemMonitor/windows/main/MainWindow.axaml.cs
+++ SystemMonitor/windows/main/MainWindow.axaml.cs
@@ -1,5 +1,6 @@
 using Avalonia.Controls;
 using System;
+using System.Collections.Generic;
 using System.Timers;
 using SystemMonitor.features.Metric;
 
@@ -7,7 +8,7 @@ namespace SystemMonitor;
 
 public partial class MainWindow : Window
 {   
-    private MetricManager metricManager;
+    private MetricManager? metricManager;
     Timer _timer;
 
     public MainWindow()
@@ -24,17 +25,39 @@ public partial class MainWindow : Window
     private void UpdateLabels(object? sender, EventArgs e)
     {
         metricManager = new MetricManager();
-        metricManager.GetData();
+        var data = metricManager.GetData();
 
-        
+        var cpuLoad = executeData(data["CPU"]["Load"]);
+        var cpuTemp = executeData(data["CPU"]["Temp"]);
+        var cpuFreq = executeData(data["CPU"]["Freq"]);
+
+        var gpuLoad = executeData(data["GPU"]["Load"]);
+        var gpuTemp = executeData(data["GPU"]["Temp"]);
+        var gpuFreq = executeData(data["GPU"]["Freq"]);
+
+        var ramLoad = executeData(data["RAM"]["Load"]);
+
+        CpuLoad.Content = cpuLoad;
+        CpuLoad.Content = cpuTemp;
+        CpuLoad.Content = cpuFreq;
+
+        GpuLoad.Content = gpuLoad;
+        GpuLoad.Content = gpuTemp;
+        GpuLoad.Content = gpuFreq;
+
+        RamLoad.Content = ramLoad;
 
         metricManager.ClearData();
     }
 
-    private float executeData()
+    private string executeData(List<float> data)
     {
+        float helpCount = 0;
+        foreach (float value in data)
+        {
+            helpCount += value;
+        }
         
+        return ((int)(helpCount/data.Count)).ToString();
     }
-
-
 }
